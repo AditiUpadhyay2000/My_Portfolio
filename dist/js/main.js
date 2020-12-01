d216bff553888bf11b41a04cd082db35130e4941
AditiUpadhyay2000 <59507503+AditiUpadhyay2000@users.noreply.github.com>
2020-12-01 14:05:25 +0530

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menubtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menunav = document.querySelector('.menu-nav');
 const menubrand = document.querySelector('.menu-branding');
-const navitems  = document.querySelectorAll('.nav-items');
+const navitems  = document.querySelectorAll('.nav-item');
 
 // Set initial state of menu
 
@@ -15,16 +15,18 @@ function togglemenu() {
     if(!showmenu)
     {
         menubtn.classList.add('close');
-        menu.classList.add('close');
-        menubrand.classList.add('close');
-        menunav.classList.add('close');
+        menu.classList.add('show');
+        menunav.classList.add('show');
+        menubrand.classList.add('show');
+        
         navitems.forEach(item => item.classList.add('show'));
     }
     else{
         menubtn.classList.remove('close');
-        menu.classList.remove('close');
-        menubrand.classList.remove('close');
-        menunav.classList.remove('close');
-        removeitems.forEach(item => item.classList.remove('show'));
+        menu.classList.remove('show');
+        menunav.classList.remove('show');
+        menubrand.classList.remove('show');
+        
+        navitems.forEach(item => item.classList.remove('show'));
     }
 }
